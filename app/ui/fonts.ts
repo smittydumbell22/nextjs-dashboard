3b4b8660aaec1cc5a2c7a52d785b92f07141236e
Cassie Smith <lilpainthorse@byui.edu>
2024-09-20 12:58:23 -0700

    Dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Inter font
+export const inter = Inter({ subsets: ['latin'] });
+
+// Lusitana font with specific weights
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
